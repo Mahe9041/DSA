0a2d1924d0d52bf29b4cdbef42554be1c448f68f
Mahesh <mahesh.official368@gmail.com>
2022-12-28 14:41:42 +0530

    iteration file

---
+++ iteration_stat.py
@@ -0,0 +1,18 @@
+#iteration statement
+#for loop
+>>> m=[] 
+>>> n=5
+>>> for i in range(n):
+...     m.append(i)    
+... 
+>>> m
+[0, 1, 2, 3, 4]
+
+#while loop
+>>> i=0 
+>>> while i<n:
+...     m.append(i)
+...     i+=1 
+... 
+>>> m
+[0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
